91c6116e54e665d380fdfc54024c1bda26f5ee83
Tapan Thaker <tapanth@uber.com>
2022-02-09 23:21:14 -0800

    fix build.sh such that it generates a fat-binary

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-clang ./dump-header-map.c -o dump-header-map
+rm -rf build
+mkdir build
+clang ./dump-header-map.c -o ./build/dump-header-map_x86_64 -target x86_64-apple-macos
+clang ./dump-header-map.c -o ./build/dump-header-map_arm64 -target arm64-apple-macos
+lipo -create ./build/dump-header-map_x86_64 ./build/dump-header-map_arm64 -o ./build/dump-header-map
 
+pushd build
+tar -cjf dump-header-map.tar.gz dump-header-map
+popd
